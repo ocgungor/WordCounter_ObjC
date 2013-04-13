fddc2fd6be01e452337f2e4d06c427fc44afc235
Oguzhan Gungor <cansingungor@gmail.com>
2013-04-14 08:09:02 +1000

    XCode Project added

---
+++ WordCounter_ObjC/WordCounter_ObjC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WordCounter_ObjC
+//
+//  Created by Oguzhan Gungor on 14/04/13.
+//  Copyright (c) 2013 Oguzhan Gungor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
